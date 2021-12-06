71c6fbdfb07f75d242761fa5ea6740fa385c3c1b
Ali Cicek <mralicicek@outlook.com>
2021-12-06 15:41:51 +0000

    2nd commit

--- script.js
+++ script.js
@@ -1,48 +1,28 @@
-// fetch('data.json')
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(function (data){
-//     appendData(data);
-//   })
-//   .catch(function (err) {
-//     console.log('error: ' + err);
-//   });
-
-
 let items = [];
 
 const addItem = (ev)=>{
-    ev.preventDefault();
+    ev.preventDefault(); //to stop form submitting
     let item = {
-        item: document.getElementById('item').value
-    }
-
-    for(let i = 0; i < items.length; i++){
-
-      var container = document.getElementById("displayData");
-
-      var listDiv = document.createElement("listItem");
-      var itemDiv = document.createElement("item");
-
-      listDiv.classList.add("listBox");
-      itemDiv.classList.add("itemField");
-      
-      listDiv.innerHTML = items[i].item;
-    
-      listDiv.appendChild(itemDiv);
-      container.appendChild(listDiv);
-
+        item: document.getElementById('toDoItem').value
     }
 
     items.push(item);
-    document.forms[0].reset();
-
-    // console.warn('added', {items});
-    // let pre = document.querySelector('#msg pre');
-    // pre.textContent = '\n' + JSON.stringify(items, '\t', 2)
-
+    document.forms[0].reset(); //clear the form for the next entries
+    
+    //saving to localStorage
+    // localStorage.setItem('ToDoList', JSON.stringify(items) );
 }
+
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('btn').addEventListener('click', addItem);
+
+    let toDoList = document.querySelector('.toDoList');
+    let toDoItems = "";
+
+    for (item of items) {
+        //console.log(people[i].name);
+        toDoList += "<li>" + item.name + "</li>";
+    }
+
+    toDoList.innerHTML = toDoItems;
 });
